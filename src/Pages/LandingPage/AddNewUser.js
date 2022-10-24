6eeaf6bcadd3a1038c9a2a8f8601507a0734eeff
Shahadat Hossain <60577101+shahadathossain4536@users.noreply.github.com>
2022-10-24 13:08:49 +0600

    ...

--- src/Pages/LandingPage/AddNewUser.js
+++ src/Pages/LandingPage/AddNewUser.js
@@ -7,7 +7,9 @@ const AddNewUser = ({ addNewUser, setAddNewUser }) => {
         onClick={() => setAddNewUser(!addNewUser)}
         className=" fixed top-0 left-0 w-full h-screen bg-[#11111185] "
       ></div>
-      <div className="fixed top-[300px] left-[115px]  py-8 px-7  overflow-y-auto  w-[945px] h-[409px] rounded-[17px] bg-[#191919]"></div>
+      <div className="fixed top-[240px] left-[600px]  py-8 px-7  overflow-y-auto  w-[945px] h-[409px] rounded-[17px] bg-[#191919]">
+        <p className="">Add new user</p>
+      </div>
     </>
   );
 };
